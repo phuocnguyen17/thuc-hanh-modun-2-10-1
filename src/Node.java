c102889d2a3b5992739a24a22d19cb744a755b77
thanhnguyenquoc15 <thanhnguyenquoc15@gmail.com>
2023-04-19 14:45:58 +0700

    bt

---
+++ src/Node.java
@@ -0,0 +1,15 @@
+
+public class Node {
+    Node next;
+    private Object data;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+
+    public Object getData() {
+        return this.data;
+    }
+
+
+}
